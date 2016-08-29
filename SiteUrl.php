0d139a73045ba1c001da134dbc9cf01ded1fc409
Steve Clay <steve@mrclay.org>
2016-08-29 11:26:55 -0400

    Adds SiteUrl::analyzeUrl

--- SiteUrl.php
+++ SiteUrl.php
@@ -30,11 +30,31 @@ class SiteUrl {
 	/**
 	 * Get a site path object if the URL is within the Elgg site, or false if not.
 	 *
-	 * @param string $url                  URL, possibly within the site
-	 * @param bool   $require_scheme_match To match, the scheme must match (e.g. http: won't match https:)
+	 * @param string $url          URL, possibly within the site
+	 * @param bool   $match_scheme Require the URL scheme to match, e.g. http: won't match https:
 	 * @return SitePath|bool
 	 */
-	public function getSitePath($url, $require_scheme_match = true) {
+	public function getSitePath($url, $match_scheme = true) {
+		$ret = $this->analyzeUrl($url);
+
+		if (!$ret || !$ret['path_within_site'] || ($ret['site_host'] !== $ret['host'])) {
+			return false;
+		}
+
+		if ($match_scheme && ($ret['scheme'] !== $ret['site_scheme'])) {
+			return false;
+		}
+
+		return new SitePath(implode('/', $ret['url_segments_within_site']));
+	}
+
+	/**
+	 * Analyze a URL.
+	 *
+	 * @param string $url URL
+	 * @return array|false
+	 */
+	public function analyzeUrl($url) {
 		$url = trim($url);
 
 		if (!preg_match('~^(https?)\\://([^/]+)(/.*|$)~', $url, $m)) {
@@ -44,29 +64,35 @@ class SiteUrl {
 		list (, $scheme, $host, $path) = $m;
 
 		$path = self::normalizePath($path);
+
 		$segments = ($path === '') ? array() : explode('/', $path);
 
-		$scheme_matches = ($scheme === $this->scheme);
-		if ($require_scheme_match && !$scheme_matches) {
-			return false;
-		}
+		$ret = [
+			'site_scheme' => $this->scheme,
+			'site_host' => $this->host,
+			'site_url_segments' => $this->url_segments,
+			'host' => $host,
+			'scheme' => $scheme,
+			'path_within_site' => false,
+			'url_segments' => $segments,
+			'url_segments_within_site' => [],
+		];
 
-		$host_matches = ($host === $this->host);
-		if (!$host_matches) {
-			return false;
-		}
-
-		while (count($this->url_segments)) {
-			$site_segment = array_shift($this->url_segments);
+		$site_segments = $this->url_segments;
+		while (count($site_segments)) {
+			$site_segment = array_shift($site_segments);
 			$segment = array_shift($segments);
 
 			if ($segment !== $site_segment) {
 				// not in site
-				return false;
+				return $ret;
 			}
 		}
 
-		return new SitePath(implode('/', $segments));
+		$ret['path_within_site'] = true;
+		$ret['url_segments_within_site'] = $segments;
+
+		return $ret;
 	}
 
 	/**
